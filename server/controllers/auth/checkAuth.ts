81ab8bbd8cf5e1724de95c991f4bbe7ea82802bf
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-04-27 16:46:32 +0300

    Use async/await

--- server/controllers/auth/checkAuth.ts
+++ server/controllers/auth/checkAuth.ts
@@ -2,19 +2,18 @@ import { Response, NextFunction } from 'express';
 import { verifyToken } from '../../utils';
 import { customError } from '../errors';
 
-const checkAuth = (req: any, res: Response, next: NextFunction) => {
+const checkAuth = async (req: any, res: Response, next: NextFunction) => {
   const { token } = req.cookies;
   if (!token) {
     next(customError('Unauthorized!', 401));
   } else {
-    verifyToken(token)
-      .then((decoded: any) => {
-        req.userInformation = decoded;
-        next();
-      })
-      .catch(() => {
-        res.status(401).clearCookie('token').json({ msg: 'Unauthorized!' });
-      });
+    try {
+      const decoded: any = await verifyToken(token);
+      req.userInformation = decoded;
+      next();
+    } catch (e) {
+      res.status(401).clearCookie('token').json({ msg: 'Unauthorized!' });
+    }
   }
 };
 
