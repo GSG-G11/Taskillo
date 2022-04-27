288201e730fb38c1e24e002db9af87743fdb7c84
karam-zomlut <zomlutk@gmail.com>
2022-04-27 15:31:58 +0300

    Create Users Table Relates #9

--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -1,1 +1,18 @@
- 
+BEGIN;
+
+DROP TABLE IF EXISTS 'Users', 'Projects', 'Sections', 'Tasks', 'User_Tasks', 'User_Projects' CASCADE;
+
+CREATE TABLE 'Users' (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  firstName VARCHAR(255),
+  lastName VARCHAR(255),
+  email VARCHAR(255) NOT NULL,
+  phoneNumber VARCHAR(255),
+  password VARCHAR(255) NOT NULL,
+  birthDate DATE,
+  bio VARCHAR(255),
+);
+
+
+
