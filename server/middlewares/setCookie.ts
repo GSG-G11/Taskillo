d39ac2971dea3f44b57d7136225b9c3573a6b3d7
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-04-27 15:12:25 +0300

    Create setCookie middleware

--- server/middlewares/setCookie.ts
+++ server/middlewares/setCookie.ts
@@ -1,1 +1,14 @@
- 
+const setCookie = (
+  response: any,
+  token: any,
+  statusCode: number,
+  message: string,
+) => response
+  .cookie('token', token, {
+    httpOnly: true,
+    secure: false,
+  })
+  .status(statusCode)
+  .json({ msg: message });
+
+export default setCookie;
