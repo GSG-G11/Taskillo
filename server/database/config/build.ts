e6983a7517c03c00835678f27ad1e988dbefe07a
karam-zomlut <zomlutk@gmail.com>
2022-04-28 01:12:01 +0300

    Create Build.ts File

--- server/database/config/build.ts
+++ server/database/config/build.ts
@@ -1,1 +1,15 @@
- 
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import connection from './connection';
+
+const dbBuild = async () => {
+  const buildFile = readFileSync(join(__dirname, 'build.sql')).toString();
+  const fakeDataFile = readFileSync(join(__dirname, 'fakedata.sql')).toString();
+  try {
+    await connection.query(buildFile + fakeDataFile);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default dbBuild;
