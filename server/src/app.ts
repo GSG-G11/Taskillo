926c2238bb5d728fc24e933a7d8e952974e8ef6b
amasri88 <amran.man99@gmail.com>
2022-04-28 23:08:29 +0300

    set up the server relats #22

---
+++ server/src/app.ts
@@ -0,0 +1,31 @@
+import { join } from 'path';
+import express from 'express';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import router from './routes';
+
+const app = express();
+
+const { env: { PORT, NODE_ENV } } = process;
+
+app.use(router);
+
+app.set('port', PORT || 4000);
+app.disable('x-powered-by');
+
+app.use([compression(), express.urlencoded({ extended: true }), express.json()]);
+
+if (NODE_ENV === 'development') {
+  app.get('/', (req, res) => {
+    res.json({ message: 'The server is running' });
+  });
+}
+
+if (NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+export default app;
