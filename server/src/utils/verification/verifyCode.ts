b4697412d14ec6663a5d5af5b5aa51ad0535c3a1
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-04-30 00:32:51 +0300

    Conver verifyCode middleware to promise

--- server/src/utils/verification/verifyCode.ts
+++ server/src/utils/verification/verifyCode.ts
@@ -1,4 +1,3 @@
-import { NextFunction } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,19 +8,15 @@ const {
 
 const twilioClient = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-const verifyCode = async (req: any, res: any, next: NextFunction) => {
-  const { email, code } = req.body;
-  try {
-    const verification = await twilioClient.verify
-      .services(process.env.TWILIO_VERIFY_SERVICE_SID)
-      .verificationChecks.create({ to: `${email}`, code });
-    if (verification.status === 'approved') {
-      res.status(200).json({ message: 'Account verified successfully' });
-    }
-    res.status(400).json({ message: 'Invalid verification code' });
-  } catch (error) {
-    next(error);
-  }
-};
+const verifyCode = (email: string, code: number) => new Promise((resolve, reject) => {
+  twilioClient.verify
+    .services(process.env.TWILIO_VERIFY_SERVICE_SID)
+    .verificationChecks.create({ to: `${email}`, code }, (error: Error, verification: any) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(verification.status);
+    });
+});
 
 export default verifyCode;
