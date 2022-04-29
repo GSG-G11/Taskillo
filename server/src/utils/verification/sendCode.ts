64659cb6b989c124c1ea5ad96aa3555dbdf108e0
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-04-30 00:14:44 +0300

    Convert sendCode middleware to promise

--- server/src/utils/verification/sendCode.ts
+++ server/src/utils/verification/sendCode.ts
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-import { NextFunction } from 'express';
 
 dotenv.config();
 
@@ -9,28 +8,24 @@ const {
 
 const twilioClient = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-const sendCode = async (req: any, res: any, next: NextFunction) => {
-  const { email } = req.body;
-  try {
-    const verification = await twilioClient.verify
-      .services(TWILIO_VERIFY_SERVICE_SID)
-      .verifications.create({
+const sendCode = (email: string) => new Promise((resolve, reject) => {
+  twilioClient.verify
+    .services(TWILIO_VERIFY_SERVICE_SID)
+    .verifications.create(
+      {
         channelConfiguration: {
           from: 'na86@smail.ucas.edu.ps', // we will replace it later with the real one
         },
         to: `${email}`,
         channel: 'email',
-      });
-    if (verification.status === 'pending') {
-      res.status(200).json({
-        message: 'The verification code has been sent to your email',
-      });
-    } else {
-      res.status(400).json({ message: 'Error sending verification code' });
-    }
-  } catch (error) {
-    next(error);
-  }
-};
+      },
+      (error: Error, verification: any) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(verification.status);
+      },
+    );
+});
 
 export default sendCode;
