0be1dc0ede25954542101c0a82d88ce81d197d90
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-04-30 17:06:06 +0300

    Create add to user_projects table query

--- server/src/database/quieres/projects/addUserProjectsQuery.ts
+++ server/src/database/quieres/projects/addUserProjectsQuery.ts
@@ -1,12 +1,18 @@
 import connection from '../../config/connection';
 
-const addUserProjectsQuery = (userId: number, projectId: number, role:string) => {
-  connection.query({
+const addUserProjectsQuery = (
+  userId: number,
+  projectId: number,
+  role: string,
+) => {
+  const sql = {
     text: `
-      INSERT INTO user_projects (userId, projectId, role) VALUES ($1, $2, $3);
-      `,
+        INSERT INTO user_projects (userId, projectId, role) VALUES ($1, $2, $3);
+        `,
     values: [userId, projectId, role],
-  });
+  };
+
+  connection.query(sql);
 };
 
 export default addUserProjectsQuery;
