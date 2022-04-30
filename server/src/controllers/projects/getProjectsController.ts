9158904322b0d098d6aee1f214cda468f42af22e
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-04-30 11:30:35 +0300

    Update get projects controller

--- server/src/controllers/projects/getProjectsController.ts
+++ server/src/controllers/projects/getProjectsController.ts
@@ -3,13 +3,13 @@ import { getProjectsQuery } from '../../database';
 
 const getProjects = async (req: Request, res: Response) => {
   const { userId } = req.body;
-  const { rows } = await getProjectsQuery(userId);
-
-  if (!rows) {
-    return res.status(204).json({ message: 'No data found' });
+  try {
+    const { rows } = await getProjectsQuery(userId);
+    if (!rows) return res.status(204).json({ message: 'No data found' });
+    return res.status(200).json({ data: rows, message: 'Success' });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
-
-  return res.status(200).json({ data: rows, message: 'Success' });
 };
 
 export default getProjects;
