8f62ed17577b9045e4693ef0e3a09e4d1e099d21
amasri88 <amran.man99@gmail.com>
2022-04-30 23:29:21 +0300

    edit deleteTaskQuery

--- server/src/database/ quieres/tasks/deleteTaskQuery.ts
+++ server/src/database/ quieres/tasks/deleteTaskQuery.ts
@@ -1,9 +1,9 @@
 import connection from '../../config/connection';
 
-const deleteTaskQuery = (userId: number) => {
+const deleteTaskQuery = (id: number) => {
   return connection.query({
-    text: 'DELETE FROM tasks join user_tasks on tasks.id = user_tasks.taskId join users on users.id = user_task.userId WHERE userId=$1',
-    values: [userId],
+    text: 'DELETE FROM tasks join user_tasks on tasks.id = user_tasks.taskId join users on users.id = user_task.userId WHERE id=$1',
+    values: [id],
   });
 };
 
