9259e1b7a59e899c6bf9c86654f1421dd37698ad
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-05-01 15:04:06 +0300

    Update the condition

--- server/src/controllers/projects/getProjectsController.ts
+++ server/src/controllers/projects/getProjectsController.ts
@@ -4,8 +4,8 @@ import { getProjectsQuery } from '../../database';
 const getProjects = async (req: Request, res: Response) => {
   const { userId } = req.body;
   try {
-    const { rows } = await getProjectsQuery(userId);
-    if (!rows) return res.status(204).json({ message: 'No data found' });
+    const { rows, rowCount } = await getProjectsQuery(userId);
+    if (!rowCount) return res.status(204).json({ message: 'No data found' });
     return res.status(200).json({ data: rows, message: 'Success' });
   } catch (error) {
     return res.status(500).json({ message: error.message });
