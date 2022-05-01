bc5caa392e5e358974779a91a1a65f3d44d20771
amasri88 <amran.man99@gmail.com>
2022-05-02 01:21:49 +0300

    fix code based on Nizar comments

--- server/src/controllers/tasks/deleteTaskController.ts
+++ server/src/controllers/tasks/deleteTaskController.ts
@@ -2,11 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import { deleteTaskQuery } from '../../database/ quieres';
 
 const deleteTaskController = async (req: Request, res: Response, next: NextFunction) => {
-  const { id } = req.body;
+  const { id } = req.params;
 
   try {
     await deleteTaskQuery(id);
-    res.status(204).json({ status: 204, message: 'The post deleted successfuly' });
+    res.status(204).json({ message: 'The post deleted successfuly' });
   } catch (err) {
     next(err);
   }
--- server/src/database/ quieres/tasks/deleteTaskQuery.ts
+++ server/src/database/ quieres/tasks/deleteTaskQuery.ts
@@ -1,6 +1,6 @@
 import connection from '../../config/connection';
 
-const deleteTaskQuery = (id: number) => {
+const deleteTaskQuery = (id: string) => {
   return connection.query({
     text: 'DELETE FROM tasks join user_tasks on tasks.id = user_tasks.taskId join users on users.id = user_task.userId WHERE id=$1',
     values: [id],
