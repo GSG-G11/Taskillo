7a54ac2d34b91839c45c8f98993efbba8c226518
Maram Ismail <maramismail2015@gmail.com>
2022-05-03 10:28:22 +0300

    edit  condition

--- server/src/controllers/projects/tasks/getTasks.ts
+++ server/src/controllers/projects/tasks/getTasks.ts
@@ -4,8 +4,8 @@ import getTasksQuery from '../../../database/ quieres';
 const getTasks = async (req: any, res: Response) => {
   const { userId } = req.userInformation;
   try {
-    const { rows } = await getTasksQuery(userId);
-    if (!rows) return res.status(204).json({ msg: 'No Task Yet ' });
+    const { rows, rowCount} = await getTasksQuery(userId);
+    if (!rowCount) return res.status(204).json({ msg: 'No Task Yet ' });
     return res.status(200).json({ data: rows, msg: 'Success' });
   } catch (error) {
     return res.status(500).json({ msg: error.message });
