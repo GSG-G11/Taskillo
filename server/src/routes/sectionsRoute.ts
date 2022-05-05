e4b20b7a46e95d2538ec251fa5b55f16d44c7935
karam-zomlut <zomlutk@gmail.com>
2022-05-06 01:19:57 +0300

    Create Delete Section Route

--- server/src/routes/sectionsRoute.ts
+++ server/src/routes/sectionsRoute.ts
@@ -1,8 +1,9 @@
-import { addSectionController } from '../controllers';
+import { addSectionController, deleteSectionController } from '../controllers';
 import { checkAuth } from '../middlewares';
 
 const sectionsRoute = require('express').Router();
 
 sectionsRoute.post('/section', addSectionController);
+sectionsRoute.delete('/section/:id', deleteSectionController);
 
 export default sectionsRoute;
