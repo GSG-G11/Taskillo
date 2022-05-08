58e3ffea39caa40bba6466aaeaba6ae3ed5d0dc6
amasri88 <amran.man99@gmail.com>
2022-05-08 12:16:35 +0300

    create interfase in editTaskQuery

--- server/src/controllers/tasks/editTaskController.ts
+++ server/src/controllers/tasks/editTaskController.ts
@@ -4,11 +4,12 @@ import { editTaskQuery } from '../../database';
 const editTask = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
+    const taskId = Number(id);
     const {
       name, description, priority, endDate, status,
     } = req.body;
     const { rows } = await editTaskQuery({
-      id, name, description, priority, endDate, status,
+      id: taskId, name, description, priority, endDate, status,
     });
     res.status(200).json({ data: rows[0], message: 'The task updated successfully!' });
   } catch (error) {
--- server/src/database/quieres/tasks/editTaskQuery.ts
+++ server/src/database/quieres/tasks/editTaskQuery.ts
@@ -1,20 +1,18 @@
 import connection from '../../config/connection';
 
-// interface Task {
-//   id: number;
-//   name: string;
-//   description: string;
-//   priority: string;
-//   endDate: string;
-//   status: string;
-// }
+interface Task {
+  id: number;
+  name: string;
+  description: string;
+  priority: string;
+  endDate: string;
+  status: string;
+}
 
-const editTaskQuery = ({
-  id, name, description, priority, endDate, status,
-}) => {
+const editTaskQuery = (task: Task) => {
   const sql = {
     text: 'UPDATE tasks SET name = $2, description = $3, priority = $4, endDate = $5, status = $6 join user_tasks on task.id = user_tasks.taskId join user on user_tasks.userId = user.id WHERE id = $1 RETURNING *',
-    values: [id, name, description, priority, endDate, status],
+    values: [task.id, task.name, task.description, task.priority, task.endDate, task.status],
   };
   return connection.query(sql);
 };
