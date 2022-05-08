2a9a986e2e7078fc17c55593506187305cc3c4f6
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-05-08 09:37:50 +0300

    Handle get specific project controller

--- server/src/controllers/projects/getSpecificProjectController.ts
+++ server/src/controllers/projects/getSpecificProjectController.ts
@@ -7,7 +7,7 @@ const getSpecificProject = async (req: Request, res: Response) => {
     const { rows, rowCount } = await getSpecificProjectQuery(+id);
     const { rows: staff } = await getMembersQuery(+id);
     if (!rowCount) return res.status(204).json({ message: 'No data found' });
-    return res.status(200).json({ data: rows, staff, message: 'Success' });
+    return res.status(200).json({ data: { rows, staff }, message: 'Success' });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
