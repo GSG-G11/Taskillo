89d5a9a7074d9c8460f6a252943f6ac2b17739e0
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-05-08 20:41:10 +0300

    Refactor edit project controller

--- server/src/controllers/projects/editProject.ts
+++ server/src/controllers/projects/editProject.ts
@@ -1,20 +1,12 @@
-import { NextFunction, Response, Request } from 'express';
+import { Response, Request } from 'express';
 import { editProjectQuery } from '../../database';
 
-const editProject = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+const editProject = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { name, description } = req.body;
   const projectId = Number(id);
-  try {
-    const { rows } = await editProjectQuery(projectId, name, description);
-    res.status(200).json({ data: rows[0], message: 'Project edited' });
-  } catch (error) {
-    next(error);
-  }
+  const { rows } = await editProjectQuery(projectId, name, description);
+  res.status(200).json({ data: rows[0], message: 'Project edited' });
 };
 
 export default editProject;
