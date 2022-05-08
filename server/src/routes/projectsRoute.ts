21e807fb4ead20e059b259b78c16ae55398c33c1
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-05-08 21:05:54 +0300

    Refactor checkAuth Controller

--- server/src/routes/projectsRoute.ts
+++ server/src/routes/projectsRoute.ts
@@ -9,7 +9,7 @@ import { asyncMiddleware, checkAuth } from '../middlewares';
 
 const projectsRoute = require('express').Router();
 
-projectsRoute.use(checkAuth);
+projectsRoute.use(asyncMiddleware(checkAuth));
 projectsRoute.post('/project', asyncMiddleware(addProject));
 projectsRoute.get('/projects', asyncMiddleware(getProjects));
 projectsRoute.delete('/project/:id', asyncMiddleware(deleteProject));
