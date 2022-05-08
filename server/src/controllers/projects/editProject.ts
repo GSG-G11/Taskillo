32a94a7d6afe60e0d584c8a84d937ac4603bbd35
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-05-08 21:09:09 +0300

    Refactor edit project query

--- server/src/controllers/projects/editProject.ts
+++ server/src/controllers/projects/editProject.ts
@@ -4,8 +4,8 @@ import { editProjectQuery } from '../../database';
 const editProject = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { name, description } = req.body;
-  const projectId = Number(id);
-  const { rows } = await editProjectQuery(projectId, name, description);
+  const projectId = +id;
+  const { rows } = await editProjectQuery({ projectId, name, description });
   res.status(200).json({ data: rows[0], message: 'Project edited' });
 };
 
