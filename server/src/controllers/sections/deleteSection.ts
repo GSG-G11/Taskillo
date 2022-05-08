7a24880908953ec5d6d00895e656e9e0208f1cec
karam-zomlut <zomlutk@gmail.com>
2022-05-08 21:18:58 +0300

    Refactor

--- server/src/controllers/sections/deleteSection.ts
+++ server/src/controllers/sections/deleteSection.ts
@@ -4,7 +4,7 @@ import { deleteSectionQuery, getOneSectionQuery } from '../../database';
 const deleteSection = async (req: Request, res: Response) => {
   const id = +req.params.id;
   const { rowCount } = await getOneSectionQuery(id);
-  if (rowCount > 0) {
+  if (rowCount) {
     await deleteSectionQuery(id);
     res.status(200).json({
       message: 'Section deleted successfully',
