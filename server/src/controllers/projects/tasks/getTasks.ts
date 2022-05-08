e7c0ad1dfe8553be236db6950205df02536ce43d
Maram Ismail <maramismail2015@gmail.com>
2022-05-08 11:11:59 +0300

    edit msg to masssge

--- server/src/controllers/projects/tasks/getTasks.ts
+++ server/src/controllers/projects/tasks/getTasks.ts
@@ -4,8 +4,8 @@ const getTasks = async (req: any, res: any) => {
   const { id } = req.userInformation;
   try {
     const { rows, rowCount } = await getTasksQuery(id);
-    if (!rowCount) return res.status(204).json({ msg: 'No Task Yet ' });
-    return res.status(200).json({ data: rows, msg: 'Success' });
+    if (!rowCount) return res.status(204).json({ massage: 'No Task Yet ' });
+    return res.status(200).json({ data: rows, massage: 'Success' });
   } catch (error) {
     return res.status(500).json({ msg: error.massage });
   }
