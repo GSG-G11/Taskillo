3700f4e5c3055c35545ccb6b40a850c4517ab66f
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-05-08 20:42:32 +0300

    Refactor get specific project controller

--- server/src/controllers/projects/getSpecificProject.ts
+++ server/src/controllers/projects/getSpecificProject.ts
@@ -3,14 +3,10 @@ import { getSpecificProjectQuery, getMembersQuery } from '../../database';
 
 const getSpecificProject = async (req: Request, res: Response) => {
   const { id } = req.params;
-  try {
-    const { rows, rowCount } = await getSpecificProjectQuery(+id);
-    const { rows: staff } = await getMembersQuery(+id);
-    if (!rowCount) return res.status(204).json({ message: 'No data found' });
-    return res.status(200).json({ data: { rows, staff }, message: 'Success' });
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
+  const { rows, rowCount } = await getSpecificProjectQuery(+id);
+  const { rows: staff } = await getMembersQuery(+id);
+  if (!rowCount) return res.status(204).json({ message: 'No data found' });
+  return res.status(200).json({ data: { rows, staff }, message: 'Success' });
 };
 
 export default getSpecificProject;
