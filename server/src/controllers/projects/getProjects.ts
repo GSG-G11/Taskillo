eb6d9ac740b4775a04798a734e86838b8c235597
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-05-08 20:41:49 +0300

    Refactor get project controller

--- server/src/controllers/projects/getProjects.ts
+++ server/src/controllers/projects/getProjects.ts
@@ -1,15 +1,11 @@
-import { Response, Request } from 'express';
+import { Response } from 'express';
 import { getProjectsQuery } from '../../database';
 
-const getProjects = async (req: Request, res: Response) => {
+const getProjects = async (req: any, res: Response) => {
   const { id } = req.userInformation;
-  try {
-    const { rows, rowCount } = await getProjectsQuery(id);
-    if (!rowCount) return res.status(204).json({ message: 'No data found' });
-    return res.status(200).json({ data: rows, message: 'Success' });
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
+  const { rows, rowCount } = await getProjectsQuery(id);
+  if (!rowCount) return res.status(204).json({ message: 'No data found' });
+  return res.status(200).json({ data: rows, message: 'Success' });
 };
 
 export default getProjects;
