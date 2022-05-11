c482750a4a3f50912b2f4db34925ae17be516e1a
amasri88 <amran.man99@gmail.com>
2022-05-11 23:02:43 +0300

    fix response message

--- server/src/controllers/tasks/getTasks.ts
+++ server/src/controllers/tasks/getTasks.ts
@@ -3,7 +3,7 @@ import { getTasksQuery } from '../../database';
 const getTasks = async (req: any, res: any) => {
   const { id } = req.userInformation;
   const { rows, rowCount } = await getTasksQuery(id);
-  if (!rowCount) return res.status(204).json({ massage: 'No Task Yet ' });
+  if (!rowCount) return res.status(204).json({ message: 'No Task Yet ' });
   return res.status(200).json({ data: rows, message: 'Success' });
 };
 
