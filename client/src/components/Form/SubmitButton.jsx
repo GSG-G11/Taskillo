5157914728b1ad77473daf5a76a6ed37977736a5
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-05-13 17:30:48 +0300

    Create submitButton Component

---
+++ client/src/components/Form/SubmitButton.jsx
@@ -0,0 +1,9 @@
+const SubmitButton = ({ title }) => {
+  return (
+    <div className="form-group">
+      <button type="submit">{title}</button>
+    </div>
+  );
+};
+
+export default SubmitButton;
