2dd199b91f3e3a41098a1f5c36fda5fecb2d4626
Maram Ismail <maramismail2015@gmail.com>
2022-05-14 11:48:37 +0300

    add logo image and edit in style css file

--- client/src/pages/SignUp/signUp.jsx
+++ client/src/pages/SignUp/signUp.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Form from '../../components/Form/Form'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './signup.css'
+import logo from '../../images/logo.svg'
 import { validationSignUpSchema } from '../../utils'
 import { FormField, SubmitButton } from '../../components';
 
@@ -23,19 +24,23 @@ export default function Signup() {
   return (
     <div className='d-flex mt-5 ' >
        <div className="col-6 ms-5">
-         <h1 className='text-white mb-5 '>Taskillo</h1>
+        <div className="top-header d-flex align-items-center">
+        <img src={logo} alt="logo website" className='mb-5' />
+       
+       <h1 className='text-white mb-5 '>Taskillo</h1>
+        </div>
          <span className='text-white mb-2'>Start For Free</span>
          <h2 className='text-white mb-4 '>Create an account</h2>
        <Form 
         initialValues={ {username:'', code:'',password:''}}
         validationSchema= {validationSignUpSchema}
         onSubmit={handleSubmit} >
-        <FormField  type='text' name='username' placeholder='Enter your username' className='mb-3 w-50 text-white'/>
-        <FormField type='password' name='password' placeholder='Enter your password' className='mb-3 w-50 text-white' />
-        <FormField  type='number' name='code' placeholder='Enter verification code' className='mb-3 w-50 text-white'/>
+        <FormField  type='text' name='username' placeholder='Enter your username' className='mb-3 w-50 text-white shadow-lg '/>
+        <FormField type='password' name='password' placeholder='Enter your password' className='mb-3 w-50 text-white shadow-lg' />
+        <FormField  type='number' name='code' placeholder='Enter verification code' className='mb-3 w-50 text-white shadow-lg'/>
         <SubmitButton  title='Sign Up' className='btn btn-primary w-50 mb-3'/>
         <SubmitButton  title='Sign up with Google' className='btn signup-google w-50 mb-3'/>
-         <p>Already have an account ? <a href="#"> Sign in </a> </p>
+         <p className='signup-text'>Already have an account ? <a href="#" className='text-decoration-none'> Sign in </a> </p>
 
     </Form>
        </div>
