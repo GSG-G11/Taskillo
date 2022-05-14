a0c503d18c77cd27d8c43d819c7669c07531fcff
amasri88 <amran.man99@gmail.com>
2022-05-14 23:51:14 +0300

    refactor my code to start with bootstrap

--- client/src/components/forms/index.jsx
+++
@@ -1,6 +0,0 @@
-import { Form } from "./Form";
-import { FormField } from "./FormField";
-import { TextError } from "./TextError";
-import { SubmitButton } from "./SubmitButton";
-
-export { Form, FormField, TextError, SubmitButton };
