a49083f77b2183091bd8b4e6ba4e86d6f0cf088e
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-05-17 12:10:46 +0300

    Get the name from state

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import useReducer from './state/user';
+
+const store = configureStore({
+  reducer: {
+    user: useReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Navbar } from '../../components';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+  const userInfo = useSelector((state) => state.user.value);
   return (
     <div className="page d-flex flex-column">
-      <Navbar title="Good Morning, Nada" />
+      <Navbar title={`Good Morning, ${userInfo.username}`} />
     </div>
   );
 };
