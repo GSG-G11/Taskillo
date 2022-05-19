c0b09c0ba09cd085509cd90534eb84335ed274e1
karam-zomlut <zomlutk@gmail.com>
2022-05-19 16:09:57 +0300

    Use Greeting Function in Home

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Navbar from '../../components/Navbar';
 import { useSelector } from 'react-redux';
 import Sidebar from '../../components/Sidebar';
+import generateGreeting from '../../utils/generateGreeting';
 
 const Home = () => {
   const userInfo = useSelector((state) => state.user.value);
   const { open } = useSelector((state) => state.sidebar.value);
+  const greeting = generateGreeting();
   return (
     <div className="page-container">
       <Sidebar />
@@ -14,7 +16,7 @@ const Home = () => {
           open ? 'main-page' : 'main-page close'
         }
       >
-        <Navbar title={`Good Morning, ${userInfo.username}`} />
+        <Navbar title={`${greeting}, ${userInfo.username}`} />
       </main>
     </div>
   );
