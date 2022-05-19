bbb21dbd6f90a114ab3117f82f5a2f4d30580915
karam-zomlut <zomlutk@gmail.com>
2022-05-19 12:05:56 +0300

    Edit id in projects Folder

--- server/src/controllers/projects/editProject.ts
+++ server/src/controllers/projects/editProject.ts
@@ -2,9 +2,9 @@ import { Response, Request } from 'express';
 import { editProjectQuery } from '../../database';
 
 const editProject = async (req: Request, res: Response) => {
-  const { id } = req.params;
+  const { projectid } = req.params;
   const { name, description } = req.body;
-  const projectId = +id;
+  const projectId = +projectid;
   const { rows } = await editProjectQuery({ projectId, name, description });
   res.status(200).json({ data: rows[0], message: 'Project edited' });
 };
--- server/src/controllers/projects/getSpecificProject.ts
+++ server/src/controllers/projects/getSpecificProject.ts
@@ -2,9 +2,9 @@ import { Response, Request } from 'express';
 import { getSpecificProjectQuery, getMembersQuery } from '../../database';
 
 const getSpecificProject = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { rows, rowCount } = await getSpecificProjectQuery(+id);
-  const { rows: staff } = await getMembersQuery(+id);
+  const { projectid } = req.params;
+  const { rows, rowCount } = await getSpecificProjectQuery(+projectid);
+  const { rows: staff } = await getMembersQuery(+projectid);
   if (!rowCount) return res.status(204).json({ message: 'No data found' });
   return res.status(200).json({ data: { rows, staff }, message: 'Success' });
 };
