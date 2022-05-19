3f7ae0ff187188cf2bb1eb00777f72a2c0d743b2
Maram Ismail <maramismail2015@gmail.com>
2022-05-19 18:41:13 +0300

    edit getTask route /delete projectid from route

--- server/src/routes/tasksRoute.ts
+++ server/src/routes/tasksRoute.ts
@@ -10,9 +10,9 @@ import {
 
 const tasksRoute = express.Router();
 tasksRoute.use(asyncMiddleware(checkAuth));
+tasksRoute.get('/tasks', asyncMiddleware(getTasks));
 tasksRoute.param('projectid', checkMember);
 tasksRoute.post('/project/:projectid/task', asyncMiddleware(addTask));
-tasksRoute.get('/project/:projectid/tasks', asyncMiddleware(getTasks));
 tasksRoute.get('/project/:projectid/section/:id/tasks', asyncMiddleware(getTasksSection));
 tasksRoute.put('/project/:projectid/task/:id', asyncMiddleware(editTask));
 tasksRoute.delete('/project/:projectid/task/:id', asyncMiddleware(deleteTask));
