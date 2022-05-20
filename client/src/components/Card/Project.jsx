335bfcce41f215d2b89f2cdd3ca8d70877ff18d1
amasri88 <amran.man99@gmail.com>
2022-05-20 20:34:41 +0300

    create projectCard component

--- client/src/components/Card/Project.jsx
+++ client/src/components/Card/Project.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text } from '../UI'
 import { VscCircleFilled } from "react-icons/vsc";
 import styled from 'styled-components';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { setProjects } from '../../state/project';
 
 export default function ProjectCard() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchProjectsByUserId = async () => {
+      try {
+        const res = await axios.get('/api/v1/projects');
+        if (res.status === 200) {
+          dispatch(setProjects(res.data.data))
+        }
+      }
+      catch (error) {
+        console.log(error);
+      }
+
+    }
+    fetchProjectsByUserId();
+
+  }, [dispatch])
+
+  const projects = useSelector((state) => state.project.value);
+
+
   return (
     <Card className='project mt-2'>
       <div className='ms-4 mt-2'>
-        <Text text={'Projects'} className={'fw-bold text-white fs-3'} />
+        <Text text={'My  Projects'} className={'fw-bold text-white fs-3'} />
       </div>
 
       <div className='mt-2 ms-4 d-flex flex-column'>
-        <div className='text-white mt-2 fs-5' >
-          <VscCircleFilled className='text-white me-2' />
-          Design profile page
-        </div>
-        <div className='text-white mt-2 fs-5' >
-          <VscCircleFilled className='text-white me-2' />
-          Design profile page
-        </div>
-        <div className='text-white mt-2 fs-5' >
-          <VscCircleFilled className='text-white me-2' />
-          Design profile page
-        </div>
+        {projects.length ? projects.map((project, index) => (
+          <div className='text-white mt-2 fs-5' key={index}>
+            <VscCircleFilled className='text-white me-2' />
+            {project.name}
+          </div>
+        )) : <div className='text-white mt-5 fs-5 ms-3'>
+          You don't have any project
+        </div>}
 
       </div>
 
