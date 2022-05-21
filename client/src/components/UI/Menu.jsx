8c9341729b5920de7b49227b9d10fe6fad9cc9cd
karam-zomlut <zomlutk@gmail.com>
2022-05-21 15:09:06 +0300

    Add Class Active to Menu Links

--- client/src/components/UI/Menu.jsx
+++ client/src/components/UI/Menu.jsx
@@ -10,7 +10,7 @@ function Menu() {
         <ul className="nav">
           <li className="nav-item">
             <NavLink to={`/project/${id}/overview`} className="nav-link">
-              <span className="link-name active">Overview</span>
+              <span className="link-name">Overview</span>
             </NavLink>
           </li>
           <li className="nav-item">
@@ -39,10 +39,27 @@ const Div = styled.nav`
     .nav {
       padding-left: 1rem;
       margin-top: 10px;
-      .link-name {
+      .nav-link {
         color: #b8b8b8;
+        font-weight: 500;
+        
+        .link-name{
+          position: relative;
+        }
+
         &.active {
           color: #fff;
+          .link-name::before{
+            content: '';
+            position: absolute;
+            left: 50%;
+            bottom: -.3rem;
+            width: 100%;
+            height: 3px;
+            border-radius: 1rem;
+            background-color: #FFF;
+            transform: translateX(-50%);
+          }
         }
         &:hover {
           color: #fff;
