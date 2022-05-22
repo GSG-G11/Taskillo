067f5290c29116b16052e717d5ec6b9de99ce917
Maram Ismail <maramismail2015@gmail.com>
2022-05-22 09:56:25 +0300

    edit task page , add taskDeleted function

--- client/src/pages/Task/index.jsx
+++ client/src/pages/Task/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { Navbar, TableTask } from '../../components';
 import { useDispatch, useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
-import { getTask } from '../../state/task';
+import { deleteTask, getTask } from '../../state/task';
 import axios from 'axios';
 import generateGreeting from '../../utils/generateGreeting';
 
@@ -26,13 +26,23 @@ const Task = () => {
   useEffect(() => {
     handleData();
   }, []);
+  const taskDeleted= async (id) =>{
+    try {
+      const Response = await axios.delete(`/api/v1/task/${id}`);
+      if(Response.status === 200){
+        dispatch(deleteTask(id))
+      }
+    } catch (error) {
+      console.log(error,'error');
+    }
 
+  };
   return (
     <div className='page-container'>
       <Outlet />
       <main className={open ? 'main-page' : 'main-page close'}>
         <Navbar title={`${greeting}, ${userInfo.username}`} />
-        <TableTask />
+        <TableTask taskDeleted={taskDeleted} />
       </main>
     </div>
   );
