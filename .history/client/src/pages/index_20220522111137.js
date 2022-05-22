bda67138adc3f1c4e433554481cdde1f95576baa
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-05-22 11:14:49 +0300

    Delete history folder

--- .history/client/src/pages/index_20220522111137.js
+++
@@ -1,8 +0,0 @@
-import Home from './Home';
-import Signup from './SignUp';
-import Login from './Login';
-import Staff from './Staff';
-import SendEmail from './SendEmail';
-import Project from './Project';
-
-export { Home, SendEmail, Signup, Login, Project };
