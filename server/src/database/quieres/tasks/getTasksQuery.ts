69023c18402529abb53831d4bbfb9cefc04a6525
Maram Ismail <maramismail2015@gmail.com>
2022-05-22 15:53:18 +0300

    edit modal

--- server/src/database/quieres/tasks/getTasksQuery.ts
+++ server/src/database/quieres/tasks/getTasksQuery.ts
@@ -1,9 +1,10 @@
 import connection from '../../config/connection';
 
-const getTasksQuery = (userId) => {
+const getTasksQuery = (userid ) => {
   const sql = {
-    text: 'select DISTINCT on (t.id) t.id, t.name,t.priority,t.enddate,t.status,p.name as projectname from tasks t join user_Tasks ut on ut.taskId= t.id join user_Projects up on ut.userid = up.userid join projects p on up.projectid = p.id where ut.userid = $1 ',
-    values: [userId],
+    text: `select DISTINCT on (t.id) t.id, t.name,t.priority,t.enddate,t.status,p.name as projectname from tasks t join user_Tasks ut on ut.taskId= t.id join user_Projects up on ut.userid = up.userid join projects p on up.projectid = p.id where ut.userid = $1 
+      `,
+    values: [userid],
   };
   return connection.query(sql);
 };
