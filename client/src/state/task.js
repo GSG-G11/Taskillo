4bd49edff99915ae449734a1935044f93c89c578
Maram Ismail <maramismail2015@gmail.com>
2022-05-22 10:18:30 +0300

    edit task redux ,add updateTask state

--- client/src/state/task.js
+++ client/src/state/task.js
@@ -9,12 +9,24 @@ const taskSlice = createSlice({
     },
     deleteTask: (state,action) =>{
       state.value = state.value.filter(value => value.id != action.payload)
+    },
+    updateTask:(state,action) =>{
+      console.log(state.value , action.payload , 'sate and payload');
+      state.value.map(value => {
+        if (value.id === action.payload.id){
+            value.name = action.payload.name;
+            value.priority = action.payload.priority;
+            value.status = action.payload.status;
+            value.enddate = action.payload.enddate;
+            value.projectname= action.payload.projectname;
+        }
+      })
     }
   },
 });
 
-const { getTask, deleteTask } = taskSlice.actions;
+const { getTask, deleteTask,updateTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
 
-export { taskSlice, getTask, deleteTask };
+export { taskSlice, getTask, deleteTask,updateTask };
